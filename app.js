7e9001e06fd48a9640ee0a5fe9f93e96c098ea1d
Steven Eng <steveneng@Stevens-MacBook-Pro.local>
2017-09-25 18:19:22 -0400

    changes to UX by making conversions easier to manage. Also added spacing for easier reading

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function base(units) {
+function base(units,quantity) {
     $.getJSON("https://api.fixer.io/latest?base=" + units, function(result) {
         for (var key in result.rates) {
             if (result.rates.hasOwnProperty(key)) {
-                $('.money').append("<div class='col-6 currency'>" + key + "</div>" + "<div class='currency col-6'>" + result.rates[key] + "</div>")
+                $('.money').append("<div class='col-6 currency'>" + key + "</div>" + "<div class='currency col-6 conversion'>" + parseFloat(quantity*result.rates[key]).toFixed(2) + "</div>" + "<hr>")
             }
         }
         $('.money').css('display','none');
@@ -12,18 +12,18 @@ function base(units) {
 };
 
 $(function() {
-    $(".units")
-        .keyup(function() {
-            $('.money').empty();
-            base($('.units').val().toUpperCase());
-            $('.cur').text($('.units').val().toUpperCase())
-        }).keyup();
-
     $('select').on('change', function() {
-        $('.money').empty();
+        $('.money').empty(); // empties the values so it wont appear on the bottom of the screen 
         var currency = this.value.length;
-        base(this.value.substring(currency - 3));
-        console.log(this.value.substring(currency - 3));
-        $('.cur').text(this.value.substring(currency - 3));
+        base(this.value.substring(currency - 3),$(".amount").val()); // sets the parameters of the type of curreny and amount of it
+        $('.cur').text(this.value.substring(currency - 3)); // replaces the HTML with the type of currency
     });
+
+    $(".amount")
+    .keyup(function() {
+        $('.money').empty();
+        var currency = $("select").val().length;
+        base($("select").val().substr(currency-3),$(".amount").val());
+        $('.quant').text($('.amount').val()); // the number of quanity would be put into HTML 
+    }).keyup();
 });
